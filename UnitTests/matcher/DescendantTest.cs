3ab3646be665be07996e2d3edd442bea097cd11a
Daniel Williams <danrwilliam@gmail.com>
2025-03-29 08:57:32 -0700

    Descendant Ancestor tests

--- UnitTests/matcher/DescendantTest.cs
+++ UnitTests/matcher/DescendantTest.cs
@@ -13,12 +13,50 @@ namespace SyntaxSearchUnitTests.Matcher
         [NUnit::TestCase("public class Test { private int _field; }", true)]
         [NUnit::TestCase("public class Test { }", false)]
         [NUnit::TestCase("struct Test { public int _field; }", false)]
-        public void ContainsField(string source, bool expected)
+        public void Descendant(string source, bool expected)
         {
             var matcher = Is.CompilationUnit.HasDescendant(
                 Is.ClassDeclaration.WithMembers(SyntaxList.NotEmpty()));
             var expr = SyntaxFactory.ParseCompilationUnit(source);
             NUnit::Assert.That(matcher.IsMatch(expr), NUnit::Is.EqualTo(expected));
+        }
+
+        [NUnit::TestCase("""
+            public void Test(int[] array, int[] dest, int offset)
+            {
+                dest[0] = array[0];
+
+                for (int i = 1; i < array.Length; i++)
+                {
+                    dest[i] = array[i];
+                }
+            }
+            """, 1
+            )]
+        [NUnit::TestCase("""
+            public void Test(int[] array, int[] dest, int offset)
+            {
+                dest[0] = array[0];
+            }
+            """, 0
+            )]
+        public void Ancestor(string source, int matches)
+        {
+            var expr = SyntaxFactory.ParseMemberDeclaration(source);
+            var matcher = Is.SimpleAssignmentExpression
+                .WithLeft(
+                    Is.ElementAccessExpression
+                        .WithArgumentList(Is.BracketedArgumentList.WithArguments(
+                            Is.Anything.Capture("index")
+                            )))
+                .WithRight(
+                    Is.ElementAccessExpression
+                        .WithArgumentList(Is.BracketedArgumentList.WithArguments(
+                            Does.Match("index")
+                        )))
+                .HasAncestor(Is.ForStatement);
+
+            NUnit::Assert.That(matcher.Search(expr), NUnit::Has.Exactly(matches).Items);
         }
     }
 }
